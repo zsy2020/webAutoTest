c2eee100687f25fe4b9dd91fed6c2aa4fdff35b2
zsy2020 <67780681+zsy2020@users.noreply.github.com>
2020-07-21 18:14:13 +0800

    Update baidu_page.py

--- page/baidu_page.py
+++ page/baidu_page.py
@@ -4,7 +4,7 @@ from poium import Page, PageElement, PageElements
 class BaiduPage(Page):
     search_input = PageElement(id_="kw", describe="搜索框")
     search_button = PageElement(id_="su", describe="搜索按钮")
-    settings = PageElement(link_text="设置", describe="设置下拉框")
+    settings = PageElement(id_="s-usersetting-top", describe="设置下拉框")
     search_setting = PageElement(css=".setpref", describe="搜索设置选项")
     save_setting = PageElement(css=".prefpanelgo", describe="保存设置")
 
